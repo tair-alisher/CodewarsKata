32b0d5cd4a85aa8198e6f7d0da7d5b6204b0e635
Алишер Тайрахунов <atairakhunov@stat.kg>
2017-11-22 13:26:02 +0600

    BreakCamelCase

---
+++ BreakCamelCase/BreakCamelCase/Kata.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BreakCamelCase
+{
+    public class Kata
+    {
+        public static string BreakCamelCase (string str)
+        {
+            string resultString = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (Char.IsUpper(str, i))
+                    resultString += ' ';
+                resultString += str[i];
+            }
+
+            return resultString;
+        }
+    }
+}
---
+++ BreakCamelCase/BreakCamelCaseTests/Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BreakCamelCase;
+
+namespace BreakCamelCaseTests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void TestCase ()
+        {
+            Assert.AreEqual("camel Casing", Kata.BreakCamelCase("camelCasing"));
+            Assert.AreEqual("camel Casing Test", Kata.BreakCamelCase("camelCasingTest"));
+        }
+    }
+}
