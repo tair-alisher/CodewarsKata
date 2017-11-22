81838a226cfb9ed018998e441953af810fbb31b1
Алишер Тайрахунов <atairakhunov@stat.kg>
2017-11-22 17:13:18 +0600

    Base64Encoding

---
+++ Base64Encoding/Base64Encoding/Base64Utils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace Base64Encoding
+{
+    public class Base64Utils
+    {
+        public static string ToBase64(string s)
+        {
+            byte[] bytesArr = Encoding.UTF8.GetBytes(s);
+            return Convert.ToBase64String(bytesArr);
+        }
+
+        public static string FromBase64(string s)
+        {
+            byte[] bytesArr = Convert.FromBase64String(s);
+            return Encoding.UTF8.GetString(bytesArr);
+        }
+    }
+}
---
+++ Base64Encoding/Base64EncodingTests/Base64EncodingTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Base64Encoding;
+
+namespace Base64EncodingTests
+{
+    [TestClass]
+    public class Base64EncodingTests
+    {
+        [TestMethod]
+        public void ToBase64_EncodeTests ()
+        {
+            Assert.AreEqual("dGhpcyBpcyBhIHN0cmluZyEh", Base64Utils.ToBase64("this is a string!!"));
+            Assert.AreEqual("ZWU=", Base64Utils.ToBase64("ee"));
+            Assert.AreEqual("ZQ==", Base64Utils.ToBase64("e"));
+            Assert.AreEqual("", Base64Utils.ToBase64(""));
+        }
+
+        [TestMethod]
+        public void FromBase64_DecodeTests ()
+        {
+            Assert.AreEqual("this is a string!!", Base64Utils.FromBase64("dGhpcyBpcyBhIHN0cmluZyEh"));
+            Assert.AreEqual("ee", Base64Utils.FromBase64("ZWU="));
+            Assert.AreEqual("e", Base64Utils.FromBase64("ZQ=="));
+            Assert.AreEqual("", Base64Utils.FromBase64(""));
+        }
+    }
+}
