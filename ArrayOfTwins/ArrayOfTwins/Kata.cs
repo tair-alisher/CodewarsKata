b0f22467625fa32a9249f60eb2a1cb165a179f58
Алишер Тайрахунов <atairakhunov@stat.kg>
2017-11-22 13:11:00 +0600

    ArrayOfTwins

---
+++ ArrayOfTwins/ArrayOfTwins/Kata.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArrayOfTwins
+{
+    public class Kata
+    {
+        public static bool Twins (int[] arr)
+        {
+            Dictionary<int, int> ocNum = new Dictionary<int, int>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (ocNum.ContainsKey(arr[i]))
+                    ocNum[arr[i]] += 1;
+                else
+                    ocNum.Add(arr[i], 1);
+            }
+
+            foreach (KeyValuePair<int, int> entry in ocNum)
+                if (entry.Value != 2)
+                    return false;
+
+            return true;
+        }
+    }
+}
---
+++ ArrayOfTwins/ArrayOfTwinsTests/Tests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ArrayOfTwins;
+
+namespace ArrayOfTwinsTests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void Twins_WhenArrayContainsPairs ()
+        {
+            Assert.AreEqual(true, Kata.Twins(new int[] { 1,2,3,1,2,3 }));
+        }
+
+        [TestMethod]
+        public void Twins_WhenArrayDoesNotContainParis ()
+        {
+            Assert.AreEqual(false, Kata.Twins(new int[] { 33, 5, 17, 5 }));
+            Assert.AreEqual(false, Kata.Twins(new int[] { 2, 16, 2, 17, 2, 16 }));
+        }
+    }
+}
