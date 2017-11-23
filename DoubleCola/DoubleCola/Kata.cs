85b62930710f88683d936e6001ec458eb17f86fa
tair-alisher <ur.alisher@mail.ru>
2017-11-23 12:59:42 +0600

    DoubleCola

---
+++ DoubleCola/DoubleCola/Kata.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace DoubleCola
+{
+    public class Kata
+    {
+        public static string WhoIsNext (string[] names, long n)
+        {
+            long i = n - 1;
+            while (i >= names.Length)
+                i = (i - names.Length) / 2;
+            return names[i];
+        }
+    }
+}
---
+++ DoubleCola/DoubleColaTests/KataTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DoubleCola;
+
+namespace DoubleColaTests
+{
+    [TestClass]
+    public class KataTests
+    {
+        string[] names = new string[] { "Sheldon", "Leonard", "Penny", "Rajesh", "Howard" };
+
+        [TestMethod]
+        public void WhoIsNext_WhenInputValueIsLessThanNamesArrayLength ()
+        {
+            Assert.AreEqual("Sheldon", Kata.WhoIsNext(names, 1));
+        }
+
+        [TestMethod]
+        public void WhoIsNext_WhenInputValueIsMoreThanNamesArrayLength ()
+        {
+            Assert.AreEqual("Sheldon", Kata.WhoIsNext(names, 6));
+        }
+    }
+}
