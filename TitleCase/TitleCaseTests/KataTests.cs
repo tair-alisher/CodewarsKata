ce568e2f1c0db64830ce9ccc6be2348e9c1413f7
Alisher <ur.alisher@mail.ru>
2017-11-24 22:26:49 +0600

    TitleCase

---
+++ TitleCase/TitleCaseTests/KataTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TitleCase;
+
+namespace TitleCaseTests
+{
+    [TestClass]
+    public class KataTests
+    {
+        [TestMethod]
+        public void TitleCase_WhenInputValuesAreEmpty ()
+        {
+            Assert.AreEqual("", Kata.TitleCase(""));
+        }
+
+        [TestMethod]
+        public void TitleCase_WhenMinorWordsAreEmpty ()
+        {
+            Assert.AreEqual("The Quick Brown Fox", Kata.TitleCase("the quick brown fox"));
+        }
+
+        [TestMethod]
+        public void TitleCase_Tests ()
+        {
+            Assert.AreEqual("A Clash of Kings", Kata.TitleCase("a clash of KINGS", "a an the of"));
+            Assert.AreEqual("The Wind in the Willows", Kata.TitleCase("THE WIND IN THE WILLOWS", "The In"));
+        }
+    }
+}
