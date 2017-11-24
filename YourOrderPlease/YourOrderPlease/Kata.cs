9b40711289598d6e75270abc7153057d23a0129b
Alisher <ur.alisher@mail.ru>
2017-11-24 20:48:44 +0600

    YourOrderPlease

---
+++ YourOrderPlease/YourOrderPlease/Kata.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace YourOrderPlease
+{
+    public class Kata
+    {
+        public static string Order (string words)
+        {
+            if (words.Length == 0) return "";
+
+            double numericValue;
+            Dictionary<double, string> resultDct = new Dictionary<double, string>();
+            string[] wordsArr = words.Split(' ');
+
+            foreach (string word in wordsArr)
+            {
+                foreach (char letter in word)
+                {
+                    numericValue = Char.GetNumericValue(letter);
+                    if (numericValue != (-1))
+                        resultDct.Add(numericValue, word);
+                }
+            }
+
+            return string.Join(" ", resultDct.OrderBy(item => item.Key).Select(item => item.Value));
+        }
+        
+    }
+}
---
+++ YourOrderPlease/YourOrderPleaseTests/KataTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YourOrderPlease;
+
+namespace YourOrderPleaseTests
+{
+    [TestClass]
+    public class KataTests
+    {
+        [TestMethod]
+        public void Order_Tests ()
+        {
+            Assert.AreEqual("Thi1s is2 3a T4est", Kata.Order("is2 Thi1s T4est 3a"));
+            Assert.AreEqual("Fo1r the2 g3ood 4of th5e pe6ople", Kata.Order("4of Fo1r pe6ople g3ood th5e the2"));
+        }
+
+        [TestMethod]
+        public void Order_WhenInputStringIsEmpty ()
+        {
+            Assert.AreEqual("", Kata.Order(""));
+        }
+    }
+}
