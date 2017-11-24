9d57e71253d5b5b8ea00cf227ac0f730e3132870
Alisher <ur.alisher@mail.ru>
2017-11-24 22:49:48 +0600

    TitleCase Edits

--- TitleCase/TitleCase/Kata.cs
+++ TitleCase/TitleCase/Kata.cs
@@ -9,16 +9,18 @@ namespace TitleCase
         {
             string[] words = string.IsNullOrEmpty(minorWords) ? new string[] { "" } : minorWords.Split(' ').Select(x => x.ToLower()).ToArray();
             string[] titles = title.Split(' ');
-            titles[0] = titles[0].Length > 1 ? Char.ToUpper(titles[0][0]) + titles[0].Substring(1).ToLower() : titles[0].ToUpper();
+            titles[0] = titles[0].Length > 1 ? Capitalize(titles[0]) : titles[0].ToUpper();
             
             for (int i = 1; i < titles.Length; i++)
             {
                 if (words.Contains(titles[i].ToLower()))
                     titles[i] = titles[i].ToLower();
                 else
-                    titles[i] = Char.ToUpper(titles[i][0]) + titles[i].Substring(1).ToLower();
+                    titles[i] = Capitalize(titles[i]);
             }
             return string.Join(" ", titles);
         }
+
+        public static string Capitalize (string word) => Char.ToUpper(word[0]) + word.Substring(1).ToLower();
     }
 }
