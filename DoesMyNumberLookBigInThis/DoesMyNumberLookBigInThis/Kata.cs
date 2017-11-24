f35121afe63d2df5d54865cf03768fd94d975f6b
tair-alisher <ur.alisher@mail.ru>
2017-11-24 13:38:29 +0600

    DoesMyNumberLookBigInThis

---
+++ DoesMyNumberLookBigInThis/DoesMyNumberLookBigInThis/Kata.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq;
+
+namespace DoesMyNumberLookBigInThis
+{
+    public class Kata
+    {
+        public static bool Narcissistic(int value) => value == value.ToString().Select(x => Convert.ToInt64(Math.Pow(Char.GetNumericValue(x), value.ToString().Length))).Sum();
+    }
+}
---
+++ DoesMyNumberLookBigInThis/DoesMyNumberLookBigInThisTests/KataTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DoesMyNumberLookBigInThis;
+
+namespace DoesMyNumberLookBigInThisTests
+{
+    [TestClass]
+    public class KataTests
+    {
+        [TestMethod]
+        public void Narcissistic_WhenInputNumberIsNarcissistic ()
+        {
+            Assert.AreEqual(true, Kata.Narcissistic(1), "1 is narcissistic");
+            Assert.AreEqual(true, Kata.Narcissistic(371), "371 is narcissistic");
+        }
+
+        [TestMethod]
+        public void Narcissistic_WhenInputNumberIsNotNarcissistic ()
+        {
+            Assert.AreEqual(false, Kata.Narcissistic(756), "756 is not narcissistic");
+        }
+    }
+}
