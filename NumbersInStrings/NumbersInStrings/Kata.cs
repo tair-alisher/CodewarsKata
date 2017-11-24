e41d35f8df7f8e20be85b6ddfb48e7a23b4b58c4
Alisher <ur.alisher@mail.ru>
2017-11-24 23:17:24 +0600

    NumbersInStrings

---
+++ NumbersInStrings/NumbersInStrings/Kata.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace NumbersInStrings
+{
+    public class Kata
+    {
+        public static int Solve (string s)
+        {
+            string result = "";
+            
+            foreach (char letter in s)
+            {
+                if (Char.IsDigit(letter))
+                {
+                    result += letter;
+                } else
+                {
+                    if (result.Length > 0 && result[result.Length-1] != ' ')
+                    {
+                        result += ' ';
+                    }
+                }
+            }
+            return result.Trim().Split(' ').Select(int.Parse).Max();
+        }
+    }
+}
---
+++ NumbersInStrings/NumbersInStringsTests/KataTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NumbersInStrings;
+
+namespace NumbersInStringsTests
+{
+    [TestClass]
+    public class KataTests
+    {
+        [TestMethod]
+        public void Solve_Tests ()
+        {
+            Assert.AreEqual(695, Kata.Solve("gh12cdy695m1"));
+            Assert.AreEqual(9, Kata.Solve("2ti9iei7qhr5"));
+            Assert.AreEqual(61, Kata.Solve("vih61w8oohj5"));
+            Assert.AreEqual(42, Kata.Solve("f7g42g16hcu5"));
+            Assert.AreEqual(85, Kata.Solve("lu1j8qbbb85"));
+        }
+    }
+}
