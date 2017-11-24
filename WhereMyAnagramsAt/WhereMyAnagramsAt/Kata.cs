ad9184dce12becceb3baeb9f509d88b881b4ba3f
Alisher <ur.alisher@mail.ru>
2017-11-24 21:28:43 +0600

    WhereMyAnagramsAt

---
+++ WhereMyAnagramsAt/WhereMyAnagramsAt/Kata.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WhereMyAnagramsAt
+{
+    public class Kata
+    {
+        public static List<string> Anagrams(string word, List<string> words) => words.Where(part => String.Concat(part.OrderBy(b => b)) == String.Concat(word.OrderBy(c => c))).ToList();
+    }
+}
---
+++ WhereMyAnagramsAt/WhereMyAnagramsAtTests/KataTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WhereMyAnagramsAt;
+
+namespace WhereMyAnagramsAtTests
+{
+    [TestClass]
+    public class KataTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            CollectionAssert.AreEqual(new List<string> { "a" }, Kata.Anagrams("a", new List<string> { "a", "b", "c", "d" }));
+            CollectionAssert.AreEqual(new List<string> { "carer", "arcre", "carre" }, Kata.Anagrams("racer", new List<string> { "carer", "arcre", "carre", "racrs", "racers", "arceer", "raccer", "carrer", "cerarr" }));
+        }
+    }
+}
