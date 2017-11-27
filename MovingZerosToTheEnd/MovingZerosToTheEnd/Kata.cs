10a7026c42e2ef7e6f884b1346aec28137ff9200
tair-alisher <ur.alisher@mail.ru>
2017-11-27 11:48:19 +0600

    MovingZerosToTheEnd

---
+++ MovingZerosToTheEnd/MovingZerosToTheEnd/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+namespace MovingZerosToTheEnd
+{
+    public class Kata
+    {
+        public static int[] MoveZeroes (int[] arr)
+        {
+            List<int> compArr = arr.Where(val => val != 0).ToList();
+            while (compArr.Count < arr.Length)
+                compArr.Add(0);
+            return compArr.ToArray();
+        }
+    }
+}
---
+++ MovingZerosToTheEnd/MovingZerosToTheEndTests/KataTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MovingZerosToTheEnd;
+
+namespace MovingZerosToTheEndTests
+{
+    [TestClass]
+    public class KataTests
+    {
+        [TestMethod]
+        public void MoveZeroes_Tests ()
+        {
+            CollectionAssert.AreEqual(new int[] { 1, 2, 1, 1, 3, 1, 0, 0, 0, 0 }, Kata.MoveZeroes(new int[] { 1, 2, 0, 1, 0, 1, 0, 3, 0, 1 }));
+        }
+    }
+}
