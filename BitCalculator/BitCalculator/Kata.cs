1fbbcb38b95220625e37db15afe48e9619cf85fa
tair-alisher <ur.alisher@mail.ru>
2017-11-27 10:51:54 +0600

    BitCalculator

---
+++ BitCalculator/BitCalculator/Kata.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace BitCalculator
+{
+    public static class Kata
+    {
+        public static int Calculate (string num1, string num2)
+        {
+            return CustomConverter(num1) + CustomConverter(num2);
+        }
+
+        public static int CustomConverter (string num)
+        {
+            return num.Reverse()
+                .Select((s, i) => new { i, s })
+                .Where(t => t.s == '1')
+                .Select(t => t.i)
+                .Select(t => (int)Math.Pow(2, t))
+                .Sum();
+        }
+
+        public static string Reverse (this string word)
+        {
+            char[] charWord = word.ToCharArray();
+            Array.Reverse(charWord);
+            return new string(charWord);
+        }
+    }
+}
---
+++ BitCalculator/BitCalculatorTests/KataTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BitCalculator;
+
+namespace BitCalculatorTests
+{
+    [TestClass]
+    public class KataTests
+    {
+        [TestMethod]
+        public void Calculate_Tests ()
+        {
+            Assert.AreEqual(2, Kata.Calculate("1", "1"));
+            Assert.AreEqual(4, Kata.Calculate("10", "10"));
+            Assert.AreEqual(2, Kata.Calculate("10", "0"));
+            Assert.AreEqual(3, Kata.Calculate("10", "1"));
+        }
+    }
+}
