abd1df037cc55eae66f703ed856d3493a9ab37d4
tair-alisher <ur.alisher@mail.ru>
2017-11-28 15:00:24 +0600

    ReflectionGiveMeAllMethods

---
+++ ReflectionGiveMeAllMethods/ReflectionGiveMeAllMethods/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using System.Reflection;
+
+namespace ReflectionGiveMeAllMethods
+{
+    public class Kata
+    {
+        public static string[] GetMethodNames (object TestObject)
+        {
+            if (TestObject == null) return new string[0];
+
+            MethodInfo[] objectMethods = TestObject.GetType().GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance);
+            return objectMethods.Select(method => method.Name).ToArray();
+        }
+    }
+}
---
+++ ReflectionGiveMeAllMethods/ReflectionGiveMeAllMethodsTests/KataTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReflectionGiveMeAllMethods;
+using System.Linq;
+
+namespace ReflectionGiveMeAllMethodsTests
+{
+    [TestClass]
+    public class KataTests
+    {
+        [TestMethod]
+        public void NullTest ()
+        {
+            Assert.AreEqual(0, Kata.GetMethodNames(null).Length);
+        }
+
+        [TestMethod]
+        public void NewObjectTest ()
+        {
+            var testObject = new object();
+            var methodNameArray = Kata.GetMethodNames(testObject);
+            Assert.IsTrue(methodNameArray.Contains("ToString"));
+        }
+    }
+}
